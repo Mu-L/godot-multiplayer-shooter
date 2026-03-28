d64be553159333b79a55a93db2c41dc86584f6c0
fetasty <fetasty@outlook.com>
2026-03-28 19:19:56 +0800

    feat: 玩家断连处理

--- main.gd
+++ main.gd
@@ -4,8 +4,8 @@ const PLAYER = preload("uid://dgstmloeo60yy")
 const ENEMY = preload("uid://pu2c45uixpy0")
 const MAIN_MENU = preload("uid://dkve68vq7kmiw")
 
-
-var died_peers: Dictionary[int, Player] = {}
+var player_dict: Dictionary[int, Player] = {}
+var died_peers: Array[int] = []
 
 @onready var multiplayer_spawner: MultiplayerSpawner = %MultiplayerSpawner
 @onready var player_spawn_marker: Marker2D = $PlayerSpawnMarker
@@ -19,13 +19,15 @@ func _ready() -> void:
 		player.input_peer_id = data.peer_id
 		player.global_position = player_spawn_marker.global_position
 		if is_multiplayer_authority():
-			player.died.connect(_on_player_died.bind(data.peer_id, player))
+			player.died.connect(_on_player_died.bind(data.peer_id))
+			player_dict[data.peer_id] = player
 		return player
-	_peer_ready.rpc_id(1)
 	if is_multiplayer_authority():
 		enemy_spawn_component.round_completed.connect(_on_round_completed)
+		multiplayer.peer_disconnected.connect(_on_peer_disconnected)
 	else:
 		multiplayer.server_disconnected.connect(_on_server_disconnected)
+	_peer_ready.rpc_id(1)
 
 
 @rpc("any_peer", "call_local", "reliable")
@@ -53,17 +55,26 @@ func _check_game_over() -> void:
 		_end_game()
 
 
-func _on_player_died(peer_id: int, player: Player) -> void:
-	died_peers.set(peer_id, player)
+func _on_player_died(peer_id: int) -> void:
+	died_peers.append(peer_id)
 	_check_game_over()
 
 
 func _on_round_completed() -> void:
 	for peer_id in died_peers:
-		var player := died_peers[peer_id]
+		var player := player_dict[peer_id]
 		player.revive(player_spawn_marker.global_position)
 	died_peers.clear()
 
 
 func _on_server_disconnected() -> void:
 	_end_game()
+
+
+func _on_peer_disconnected(peer_id: int) -> void:
+	print("[peer %s] on peer %s disconnected!" % [multiplayer.get_unique_id(), peer_id])
+	player_dict[peer_id].queue_free()
+	player_dict.erase(peer_id)
+	if peer_id in died_peers:
+		died_peers.erase(peer_id)
+	_check_game_over()

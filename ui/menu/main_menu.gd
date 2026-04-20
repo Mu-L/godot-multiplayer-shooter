18e291b0329a4f8f9c26a91785a293b00c1009e7
fetasty <fetasty@outlook.com>
2026-04-20 22:37:41 +0800

    fix: 调整治愈粒子特效;调整鼠标图标显示

--- ui/menu/main_menu.gd
+++ ui/menu/main_menu.gd
@@ -11,11 +11,8 @@ const OPTION_MENU = preload("uid://g2x3v6dbpxfa")
 @onready var options_button: Button = $VBoxContainer/OptionsButton
 @onready var quit_button: Button = $VBoxContainer/QuitButton
 
-const CURSOR = preload("uid://bf4bnwundf36w")
-
 
 func _ready() -> void:
-	Input.set_custom_mouse_cursor(CURSOR)
 	multiplayer.multiplayer_peer = OfflineMultiplayerPeer.new()
 	single_player_button.pressed.connect(_on_single_player_button_pressed)
 	multiplayer_button.pressed.connect(_on_multiplayer_button_pressed)
